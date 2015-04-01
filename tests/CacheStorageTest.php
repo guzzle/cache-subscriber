3af46f86d972f2cbf1b71772f3fd08121179c216
Rob Bast <rob.bast@gmail.com>
2015-04-01 14:18:09 +0200

    added some tests

--- tests/CacheStorageTest.php
+++ tests/CacheStorageTest.php
@@ -86,6 +86,40 @@ class CacheStorageTest extends \PHPUnit_Framework_TestCase
         $ttl = $getTtl->invokeArgs($cache, [$response]);
         $this->assertEquals(10, $ttl);
     }
+
+    /**
+     * Test that expires is considered when cache-control is not available.
+     */
+    public function testGetTtlExpires()
+    {
+        $expires = new \DateTime('+100 seconds');
+        $response = new Response(200, [
+            'Expires' => $expires->format(DATE_RFC1123),
+        ]);
+
+        $getTtl = $this->getMethod('getTtl');
+        $cache = new CacheStorage(new ArrayCache());
+        $ttl = $getTtl->invokeArgs($cache, [$response]);
+        $this->assertEquals(100, $ttl);
+    }
+
+    /**
+     * Test that cache-control is considered before expires.
+     */
+    public function testGetTtlCacheControlExpires()
+    {
+        $expires = new \DateTime('+100 seconds');
+        $response = new Response(200, [
+            'Expires' => $expires->format(DATE_RFC1123),
+            'Cache-control' => 'max-age=10',
+        ]);
+
+        $getTtl = $this->getMethod('getTtl');
+        $cache = new CacheStorage(new ArrayCache());
+        $ttl = $getTtl->invokeArgs($cache, [$response]);
+        $this->assertEquals(10, $ttl);
+    }
+
     /**
      * Return a protected or private method.
      *
